4cc469e3eae47acc1ceff8ca9e718641fe96fb5f
nacho <oteizanacho@gmail.com>
2022-04-10 21:49:50 -0300

    agrego login y register

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen( process.env.PORT || 3000, () =>
 app.get('/', function (req,res){
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
+
+app.get('/register', function (req,res){
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', function (req,res){
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
